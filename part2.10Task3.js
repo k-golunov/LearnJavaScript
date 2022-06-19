f16f98be7b42e35d0b461f6eff43c30fba081fd8
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-06-19 16:57:23 +0500

    Часть 2.10

---
+++ part2.10Task3.js
@@ -0,0 +1,11 @@
+var number = prompt("Введите число");
+
+if (number > 0) {
+	alert(1);
+}
+else if (number == 0) {
+	alert(0);
+}
+else{
+	alert(-1);
+}
---
+++ part2.10Task4.js
@@ -0,0 +1,8 @@
+let result;
+
+result = (a + b < 4) ? 'Мало' : 'Много';
+/*if (a + b < 4) {
+  result = 'Мало';
+} else {
+  result = 'Много';
+}*/
