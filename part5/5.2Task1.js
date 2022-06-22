ea42fa14d98fd9b89e4664958c54cf35bab81379
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-06-22 16:20:52 +0500

    Часть 5.2

---
+++ part5/5.2Task1.js
@@ -0,0 +1,4 @@
+let number1 = +prompt("Введите первое число", 0);
+let number2 = +prompt("Введите второе число", 0);
+
+alert(number1 + number2);
---
+++ part5/5.2Task4.js
@@ -0,0 +1,7 @@
+function random(min, max){
+	return (min + Math.random() * (max - min)).toFixed(0);
+}
+
+alert(random(1, 5));
+alert(random(1, 5));
+alert(random(1, 5));
