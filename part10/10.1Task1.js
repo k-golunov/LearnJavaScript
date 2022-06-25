4abdf9085fd63c9048d09e46786645e5dc53105c
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-06-25 16:58:50 +0500

    Часть 10.1 и 10.2

---
+++ part10/10.1Task1.js
@@ -0,0 +1,29 @@
+function f() {
+    try {
+        alert('начало');
+        return "result";
+    } catch (e) {
+    /// ...
+    } finally {
+        alert('очистка!');
+    }
+}
+
+f();
+
+function f() {
+  try {
+    alert('начало');
+    throw new Error("ошибка");
+  } catch (e) {
+    // ...
+    if("не могу обработать ошибку") {
+      throw e;
+    }
+
+  } finally {
+    alert('очистка!')
+  }
+}
+
+f();
